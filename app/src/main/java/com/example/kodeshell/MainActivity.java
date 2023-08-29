012b6974e08ff5bd596bd449b073595715931f9c
rifat414141 <rifatkhan.414141@gmail.com>
2023-08-29 21:42:18 +0600

    Chatting done. Changed this to see if it is working or not(Redirect from login to Userlist).

--- app/src/main/java/com/example/kodeshell/MainActivity.java
+++ app/src/main/java/com/example/kodeshell/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
