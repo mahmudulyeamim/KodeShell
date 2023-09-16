943cb34e19510023aab98a3cb59837595283c468
mahmudulhasanyeamim <mahmudulyeamim@gmail.com>
2023-09-16 13:29:22 +0600

    fixed minor details of the top bar

--- app/src/main/java/com/example/kodeshell/MainActivity.java
+++ app/src/main/java/com/example/kodeshell/MainActivity.java
@@ -1,36 +1,58 @@
 package com.example.kodeshell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+
+        openHomeFragment();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.home_button) {
+                    openHomeFragment();
+                    return true;
+                }
+                else if(item.getItemId() == R.id.profile_button) {
+                    openProfileFragment();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    private void openHelpActivity() {
-        Intent intent = new Intent(this, HelpActivity.class);
-        startActivity(intent);
+    private void openHomeFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, homeFragment).commit();
     }
 
-    private void openContestReminderActivity() {
-        Intent intent = new Intent(this, ContestReminderActivity.class);
-        startActivity(intent);
-    }
-
-    private void openNotificationActivity() {
-        Intent intent = new Intent(this, NotificationActivity.class);
-        startActivity(intent);
-    }
-
-    private void openMessageActivity() {
-        Intent intent = new Intent(this, MessageActivity.class);
-        startActivity(intent);
+    private void openProfileFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, profileFragment).commit();
     }
 }
