01c10c4ac037acbed8df7cdfbb00b8819385f8ef
mahmudulhasanyeamim <mahmudulyeamim@gmail.com>
2023-09-21 06:57:12 +0600

    Fixed profile fragmant

--- app/src/main/java/com/example/kodeshell/HomeFragment.java
+++ app/src/main/java/com/example/kodeshell/HomeFragment.java
@@ -2,17 +2,29 @@ package com.example.kodeshell;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeFragment extends Fragment {
 
+    MaterialToolbar toolbar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        toolbar = view.findViewById(R.id.home_top_app_bar);
+
+        return view;
     }
 }
