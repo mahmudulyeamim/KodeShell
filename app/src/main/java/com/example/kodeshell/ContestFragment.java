50483bbf2467a80fe4355a33b339371d2c9f2297
Istahak <sistahak900@gmail.com>
2023-11-02 01:23:26 +0600

    a new api call added for fetching codeforces contest specially

--- app/src/main/java/com/example/kodeshell/ContestFragment.java
+++ app/src/main/java/com/example/kodeshell/ContestFragment.java
@@ -1,5 +1,6 @@
 package com.example.kodeshell;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,9 @@ import org.json.JSONObject;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -31,6 +35,7 @@ public class ContestFragment extends Fragment {
     RecyclerView contestRecyclerView;
     ContestAdapter contestAdapter;
     private AtcoderAPIHelper atcoderAPIHelper;
+    private CodeforcesAPIHelper apiHelper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,39 +48,29 @@ public class ContestFragment extends Fragment {
         ArrayList<ContestDetails> list = new ArrayList<>();
 
         atcoderAPIHelper = new AtcoderAPIHelper();
+        apiHelper = new CodeforcesAPIHelper(getContext());
 
 
-        AtcoderAPIHelper.ContestListCallback contestListCallback = new AtcoderAPIHelper.ContestListCallback() {
+        apiHelper.fetchUpcomingContests(new CodeforcesAPIHelper.UserInfoListener() {
             @Override
-            public void onSuccess(JSONArray contestArray) {
-                for (int i = 0; i < contestArray.length(); i++) {
-                    try {
-                        JSONObject contestObject = contestArray.getJSONObject(i);
-                        String title = contestObject.getString("name");
-                        String startTime = contestObject.getString("start_time");
-                        int durationSeconds = contestObject.getInt("duration");
-                        String site=contestObject.getString("site");
-                        String url=contestObject.getString("url");
-                        String end_time=contestObject.getString("end_time");
-                        if(site.equals("CodeChef")){
-                            startTime=convertDateFormat(startTime);
-                            end_time=convertDateFormat(end_time);
-                            Log.d("hiii",startTime);
-                        }
-
-                        if(site.equals("CodeForces") || site.equals("AtCoder") || site.equals("LeetCode") || site.equals("CodeForces::Gym") ||  site.equals("CodeChef")) {
-                            if(isvalidcontest(end_time))
-                            {  ContestDetails contestDetails = new ContestDetails();
-
-                            if (site.equals("CodeForces") ||  site.equals("CodeForces::Gym"))
-                                contestDetails.setContestIcon(R.drawable.icon_codeforces);
-                            else if (site.equals("AtCoder"))
-                                contestDetails.setContestIcon(R.drawable.icon_atcoder);
-                            else if (site.equals("CodeChef")) {
-                                contestDetails.setContestIcon(R.drawable.icon_codechef);
-
-                            } else contestDetails.setContestIcon(R.drawable.icon_leetcode);
-
+            public void onSuccess(JSONObject response) {
+                try {
+                    JSONArray contestsArray = response.getJSONArray("result");
+                    for (int i = 0; i < contestsArray.length(); i++) {
+                        JSONObject contest = contestsArray.getJSONObject(i);
+                        String flag = contest.getString("phase");
+                        if (flag.equals("BEFORE")) {
+                            String contestName = contest.getString("name");
+                            long startTimeSeconds = contest.getLong("startTimeSeconds");
+                            int durationSeconds = contest.getInt("durationSeconds");
+                            ContestDetails ct= new ContestDetails();
+                            ct.setContestIcon(R.drawable.icon_codeforces);
+                            ct.setContestName(contestName);
+                            ct.setSttime(convertsecond(startTimeSeconds));
+                            ct.setEdtime(convertsecond(startTimeSeconds+durationSeconds));
+                            ct.setUrl("https://codeforces.com");
+                            ct.setContestDuration(convertSecondsToHoursMinutes(durationSeconds));
+                            String startTime=convertsecond(startTimeSeconds);
                             // Define the input date format
                             SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
                             inputDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Dhaka"));
@@ -103,24 +98,104 @@ public class ContestFragment extends Fragment {
                             } catch (ParseException e) {
                                 e.printStackTrace();
                             }
-                            String durationinhoursandmin = convertSecondsToHoursMinutes(durationSeconds);
-                            contestDetails.setSite(site);
+                            ct.setContestDate(contestDate);
+                            ct.setContestTime(contestTime);
+                            list.add(0,ct);
 
-                            contestDetails.setContestName(title);
-                            contestDetails.setContestDate(contestDate);
-                            contestDetails.setContestTime(contestTime);
-                            contestDetails.setUrl(url);
-                            contestDetails.setContestDuration(durationinhoursandmin);
-                            contestDetails.setSttime(startTime);
-                            contestDetails.setEdtime(end_time);
-                            contestDetails.setIsalarm(false);
-                            list.add(contestDetails);
                         }
+
+                        contestAdapter = new ContestAdapter(list, getContext());
+                        contestRecyclerView.setAdapter(contestAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                // Handle error
+            }
+        });
+
+
+        AtcoderAPIHelper.ContestListCallback contestListCallback = new AtcoderAPIHelper.ContestListCallback() {
+            @Override
+            public void onSuccess(JSONArray contestArray) {
+                for (int i = 0; i < contestArray.length(); i++) {
+                    try {
+                        JSONObject contestObject = contestArray.getJSONObject(i);
+                        String title = contestObject.getString("name");
+                        String startTime = contestObject.getString("start_time");
+                        int durationSeconds = contestObject.getInt("duration");
+                        String site = contestObject.getString("site");
+                        String url = contestObject.getString("url");
+                        String end_time = contestObject.getString("end_time");
+                        if (site.equals("CodeChef")) {
+                            startTime = convertDateFormat(startTime);
+                            end_time = convertDateFormat(end_time);
+                            Log.d("hiii", startTime);
                         }
+
+                        if ( site.equals("AtCoder") || site.equals("LeetCode") || site.equals("CodeForces::Gym") || site.equals("CodeChef")) {
+                            if (isvalidcontest(end_time)) {
+                                ContestDetails contestDetails = new ContestDetails();
+
+                                if (site.equals("CodeForces") || site.equals("CodeForces::Gym"))
+                                    contestDetails.setContestIcon(R.drawable.icon_codeforces);
+                                else if (site.equals("AtCoder"))
+                                    contestDetails.setContestIcon(R.drawable.icon_atcoder);
+                                else if (site.equals("CodeChef")) {
+                                    contestDetails.setContestIcon(R.drawable.icon_codechef);
+
+                                } else contestDetails.setContestIcon(R.drawable.icon_leetcode);
+
+                                // Define the input date format
+                                SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+                                inputDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Dhaka"));
+                                SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.US);
+                                SimpleDateFormat outputTimeFormat = new SimpleDateFormat("HH:mm:ss", Locale.US);
+
+                                String contestDate = "16 sep";
+                                String contestTime = "8:00";
+                                try {
+                                    // Parse the input date string
+                                    Date date = inputDateFormat.parse(startTime);
+
+                                    // Create a Calendar instance and add 6 hours
+                                    Calendar calendar = Calendar.getInstance();
+                                    calendar.setTime(date);
+                                    calendar.add(Calendar.HOUR_OF_DAY, 6);
+
+                                    // Get the modified Date object
+                                    Date modifiedDate = calendar.getTime();
+                                    // Format date and time components
+                                    contestDate = outputDateFormat.format(modifiedDate);
+                                    contestTime = outputTimeFormat.format(modifiedDate);
+
+
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                String durationinhoursandmin = convertSecondsToHoursMinutes(durationSeconds);
+                                contestDetails.setSite(site);
+
+                                contestDetails.setContestName(title);
+                                contestDetails.setContestDate(contestDate);
+                                contestDetails.setContestTime(contestTime);
+                                contestDetails.setUrl(url);
+                                contestDetails.setContestDuration(durationinhoursandmin);
+                                contestDetails.setSttime(startTime);
+                                contestDetails.setEdtime(end_time);
+                                contestDetails.setIsalarm(false);
+                                list.add(contestDetails);
+                            }
+                        }
+
 
                         Collections.sort(list, new Comparator<ContestDetails>() {
                             @Override
-                            public int compare(ContestDetails c1,ContestDetails c2) {
+                            public int compare(ContestDetails c1, ContestDetails c2) {
                                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.US);
                                 SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.US);
 
@@ -163,8 +238,6 @@ public class ContestFragment extends Fragment {
         atcoderAPIHelper.getUpcomingContests(contestListCallback);
 
 
-
-
         return view;
     }
 
@@ -182,9 +255,9 @@ public class ContestFragment extends Fragment {
         return formattedHours + ":" + formattedMinutes;
     }
 
-    private boolean isvalidcontest(String endtime){
+    private boolean isvalidcontest(String endtime) {
 
-        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
         Date targetDate = null;
         try {
             targetDate = dateFormat.parse(endtime);
@@ -200,8 +273,9 @@ public class ContestFragment extends Fragment {
         long targetTimeMillis = targetDate.getTime();
         long remainingMillis = targetTimeMillis - currentTimeMillis;
 
-        return (remainingMillis>0);
+        return (remainingMillis > 0);
     }
+
     public static String convertDateFormat(String inputDate) {
         String inputFormat = "yyyy-MM-dd HH:mm:ss";
         String outputFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
@@ -222,5 +296,21 @@ public class ContestFragment extends Fragment {
             e.printStackTrace();
             return "Parsing failed"; // Handle the parsing error gracefully
         }
+    }
+
+    public static String convertsecond(long second) {
+
+        long timeInMilliseconds = second * 1000L-6*3600*1000;
+
+        // Create a Date object using the milliseconds
+        Date date = new Date(timeInMilliseconds);
+
+        // Create a SimpleDateFormat object for the desired format
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+        // Format the date
+        String formattedDate = sdf.format(date);
+        return  formattedDate;
+
     }
 }
